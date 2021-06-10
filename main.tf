3bb1bb1aa9cc643d10ca5325cb2426a0d3924815
kennethacurtis <kacurtis1616@gmail.com>
2021-06-10 16:49:00 -0400

    add simple main tf for now

---
+++ main.tf
@@ -0,0 +1,16 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "be868bd4-3bf7-4511-8065-c4a0917fa077"
+  skip_provider_registration = true
+}
+
+terraform {
+    backend "remote" {
+        organization = "myshka"
+        workspaces {
+            name = "myshka-tf-tower"
+        }
+    }
+}
