0ddfbcab6ecb492c03cf2a27ab735348762074e9
kennethacurtis <kacurtis1616@gmail.com>
2021-07-27 16:56:24 -0400

    update terraform to support inputting server count

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
-variable "prefix" {
+variable "vm_prefix" {
   default = "automation_platform"
+}
+
+variable "server_count" {
+  default = "3"
+}
+
+variable "admin_username" {
+  default = "myshka"
 }
